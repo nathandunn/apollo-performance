9da3534d69c312f2abcf3a375ed05d5b7e6dc770
Nathan Dunn <ndunn@me.com>
2020-05-08 09:41:44 -0700

    refactored

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 [[ -d venv ]] || python3 -m venv venv
 source venv/bin/activate
 pip3 install -U pip -r requirements.txt
+ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/arrow.yml
+export ARROW_GLOBAL_CONFIG_PATH
